d4f82582983c994dd105afb8d005dbf513b3e1cd
IrynaMaroz <maroz.iryna@gmail.com>
2023-12-04 12:38:17 +0200

    common class added

--- Framework/POM/Common.cs
+++ Framework/POM/Common.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using System.Runtime.Serialization.Formatters;
 
 namespace Framework.POM
 {
     internal class Common
     {
+        internal static IWebElement GetElement(string locator)
+        {
+            return Driver.GetDriver().FindElement(By.XPath(locator));
+        }
+        internal static void ClickElement(string locator)
+        {
+            GetElement(locator).Click();
+        }
+        internal static void SendKeysToElement(string locator, string keys)
+        {
+            GetElement(locator).SendKeys(keys);
+        }
+        internal static string GetElementText(string locator)
+        {
+            return GetElement(locator).Text;
+        }
     }
 }
